cd5a66610cd978c0b3db5faa5e4ff37c0d0ab01f
william <william@ocard.co>
2020-12-09 18:32:53 +0800

    add test function

--- MessageBoard2.0/database/factories/PostFactory.php
+++ MessageBoard2.0/database/factories/PostFactory.php
@@ -5,7 +5,7 @@
 use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
         'body' => $faker->paragraph
--- MessageBoard2.0/tests/Unit/UserTest.php
+++ MessageBoard2.0/tests/Unit/UserTest.php
@@ -2,21 +2,65 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Post;
 
 class UserTest extends TestCase
 {
+    //重置DB
+    use RefreshDatabase;
+
+    public function testCreatPost()
+    {
+        //test send post
+        $response = $this->post(
+            route('posts.create'),
+            [
+                'title' => $this->faker->sentence,
+                'body'  => $this->faker->paragraph
+            ]
+        );
+        $response->assertStatus(200);
+    }
+
     /**
      * A basic unit test of post.
      *
      * @return void
      */
-    public function testPost()
+    public function testGetPost()
     {
-        $first = factory(Post::class)->create();
-        $second = factory(Post::class)->create();
+        //test get post
+        $response = $this->get(route('posts.show'));
+        $response->assertStatus(200);
+    }
 
-        $posts = Post::createPost();
-        $this->assertCount(2, $posts);
+    public function testGetPostById()
+    {
+        $post = factory(Post::class)->create();
+        $response = $this->get(route('posts.name',$post->id));
+        $response->assertStatus(200);
+    }
+
+    public function testEditPost()
+    {
+        //test edit post
+        $post = factory(Post::class)->create();
+        $response = $this->put(
+            route('posts.edit',$post->id),
+            [
+                'title' => $this->faker->sentence,
+                'body' => $this->faker->paragraph
+            ]
+        );
+        $response->assertStatus(200);
+    }
+
+    public function testDeletePost()
+    {
+        $post = factory(Post::class)->create();
+        $response = $this->delete(route('posts.delete',$post->id));
+        $response->assertStatus(200);
     }
 }
